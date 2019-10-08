84e6ab4d6155ad3f977aad0f3e723c027546f1c0
alex <benjamin.win@foxmail.com>
2019-10-08 21:20:34 +0800

    Added  __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# Author: Alex
+# IDE: PyCharm
+# CreateTime: 2019/10/8 21:18
+
+
+def main():
+    # ......
+    pass
+
+
+if __name__ == "__main__":
+    main()
